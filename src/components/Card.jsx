10901fdb299322273194ffa8b16d865883f8715e
WilliansonDantas <williansondantas@gmail.com>
2023-03-25 10:38:11 -0300

    feat:cria componente button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,40 +13,67 @@ class Card extends React.Component {
       cardRare,
       cardTrunfo } = this.props;
     return (
-      <form>
+      <form className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
         <p
           data-testid="name-card"
+          className="text-gray-800 font-bold text-xl mb-2 p-4"
         >
           {cardName}
         </p>
-        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <img
+          data-testid="image-card"
+          src={ cardImage }
+          alt={ cardName }
+          className="w-full object-cover h-64"
+        />
         <p
           data-testid="description-card"
+          className="text-gray-800 font-semibold text-md p-4"
         >
           {cardDescription}
         </p>
-        <p
-          data-testid="attr1-card"
+        <div
+          className="flex justify-between items-center p-4"
         >
-          { cardAttr1 }
-        </p>
-        <p
-          data-testid="attr2-card"
-        >
-          {cardAttr2}
-        </p>
-        <p
-          data-testid="attr3-card"
-        >
-          {cardAttr3}
-        </p>
-        <p
-          data-testid="rare-card"
-        >
-          {cardRare}
-        </p>
-        { cardTrunfo ? <p data-testid="trunfo-card"> Super Trunfo </p> : null }
+          <p
+            data-testid="attr1-card"
+            className="text-gray-700 font-semibold text-sm"
+          >
+            {cardAttr1}
+          </p>
+          <p
+            data-testid="attr2-card"
+            className="text-gray-700 font-semibold text-sm"
+          >
+            {cardAttr2}
+          </p>
+          <p
+            data-testid="attr3-card"
+            className="text-gray-700 font-semibold text-sm"
+          >
+            {cardAttr3}
+          </p>
+          <p
+            data-testid="rare-card"
+            className="text-gray-700 font-semibold text-sm"
+          >
+            {cardRare}
+          </p>
+        </div>
+        {
+          cardTrunfo ? (
+            <p
+              data-testid="trunfo-card"
+              className="text-white bg-yellow-400 font-bold
+              uppercase tracking-wide text-sm p-2"
+            >
+              Super Trunfo
+            </p>
+          )
+            : null
+        }
       </form>
+
     );
   }
 }
@@ -63,3 +90,26 @@ Card.propTypes = {
 };
 
 export default Card;
+
+/* <form>
+<p data-testid="name-card" >
+{cardName}
+</p>
+<img data-testid="image-card" src={ cardImage } alt={ cardName } />
+<p data-testid="description-card">
+{cardDescription}
+</p>
+<p data-testid="attr1-card" >
+{ cardAttr1 }
+</p>
+<p data-testid="attr2-card">
+{cardAttr2}
+</p>
+<p data-testid="attr3-card">
+{cardAttr3}
+</p>
+<p data-testid="rare-card">
+{cardRare}
+</p>
+{ cardTrunfo ? <p data-testid="trunfo-card"> Super Trunfo </p> : null }
+</form> */
